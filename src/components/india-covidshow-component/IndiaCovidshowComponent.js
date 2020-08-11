7b0d8da7d499acfdba59e4550d93fc0725bec65c
Gupta Priyanka (RBEI/ERD1) <Priyanka.Gupta@in.bosch.com>
2020-08-11 11:35:46 +0530

    Adding search

--- src/components/india-covidshow-component/IndiaCovidshowComponent.js
+++ src/components/india-covidshow-component/IndiaCovidshowComponent.js
@@ -12,7 +12,8 @@ export default class IndiaCovidshowComponent extends Component {
         this.state = {
             completeDetailsOfRegion: '',
             stateOrDistrictSelected: '',
-            searchList: []
+            searchList: [],
+            freshShow: false
         }
     }
     componentDidMount() {
@@ -40,9 +41,9 @@ export default class IndiaCovidshowComponent extends Component {
         // setTimeout(() => {
         //     DataStructureServices.search(this.props.stateDistrictCodeList, 'search', words);
         // }, 0);
-       setTimeout(() => {
-        const searchList = DataStructureServices.search(this.props.stateDistrictCodeList, 'search', words);
-            this.setState(state=>{
+        setTimeout(() => {
+            const searchList = DataStructureServices.search(this.props.stateDistrictCodeList, 'search', words);
+            this.setState(state => {
                 state.searchList = [];
                 state.searchList = searchList;
                 return state;
@@ -51,25 +52,33 @@ export default class IndiaCovidshowComponent extends Component {
 
     }
 
-    clearNCloseSearch(){
+    clearNCloseSearch() {
         this.setState({
             ...this.state,
             searchList: []
         })
     }
 
-    provideDataOfPlace(place){
+    provideDataOfPlace(place) {
         console.log(place);
         this.setState({
             ...this.state,
             completeDetailsOfRegion: this.props.findDetailsByCode(place.code),
-            searchList: []
+            searchList: [],
+            freshShow: true
+        }, () => {
+            setTimeout(() => {
+                this.setState({
+                    ...this.state,
+                    freshShow: false
+                })
+            }, 10);
         })
     }
 
     render() {
         const { stateInfoLoader } = this.props;
-        const { completeDetailsOfRegion, searchList } = this.state;
+        const { completeDetailsOfRegion, searchList, freshShow } = this.state;
         console.log('completeDetailsOfRegion ', completeDetailsOfRegion)
         return (
             <div>
@@ -88,136 +97,139 @@ export default class IndiaCovidshowComponent extends Component {
                         />
                     </div>
                 </Waypoint>
+                {!freshShow && <>
+                    {completeDetailsOfRegion !== '' && <>
 
-                {completeDetailsOfRegion !== '' && <>
-
-
-                    <TitleIconComponent icon="flare" title={completeDetailsOfRegion.info3.state === 'Total' ?
-                        'India' : completeDetailsOfRegion.info3.state} />
-                    <div className="main_lastUpdt">
-                        Last updated {this.props.convertDateToDate(completeDetailsOfRegion.info3.lastupdatedtime)}
-                    </div>
-
-
-                    <div className="icsc_qdvc">
-                        <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'con2', 'wayupanimation1')}>
-                            <div className="qdvc_ic clickConfirmedAnim" id="con2">
-                                {
-                                    stateInfoLoader && completeDetailsOfRegion !== '' ?
-                                        <div>
-                                            <div className="quickTitle confirmedCo">Confirmed</div>
-
-                                            <div className="confirmedCo delta">
-                                                {completeDetailsOfRegion.info3.deltaconfirmed !== '0' &&
-                                                    <><i className="material-icons  fontSize1 ">arrow_upward</i>
-                                                        {completeDetailsOfRegion.info3.deltaconfirmed}</>}
-                                            </div>
-
-                                            <div className="qvdc_nm">{completeDetailsOfRegion.info3.confirmed}</div>
-                                        </div> :
-                                        <LoaderComponent />}
-
+                        <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'cvhiu1', 'wayupanimation')}>
+                            <div id="cvhiu1">
+                                <TitleIconComponent icon="flare" title={completeDetailsOfRegion.info3.state === 'Total' ?
+                                    'India' : completeDetailsOfRegion.info3.state} />
+                                <div className="main_lastUpdt">
+                                    Last updated {this.props.convertDateToDate(completeDetailsOfRegion.info3.lastupdatedtime)}
+                                </div>
                             </div>
                         </Waypoint>
-                        <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'act2', 'wayupanimation2')}>
-                            <div className="qdvc_ic" id="act2">
 
-                                {
-                                    stateInfoLoader && completeDetailsOfRegion !== '' ?
-                                        <div>
-                                            <div className="quickTitle activeCo">Active</div>
 
-                                            <div className="activeCo delta">
-                                                {/* {completeDetailsOfRegion.info3.deltaactive !== 0 &&<>
+                        <div className="icsc_qdvc">
+                            <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'con2', 'wayupanimation1')}>
+                                <div className="qdvc_ic clickConfirmedAnim" id="con2">
+                                    {
+                                        stateInfoLoader && completeDetailsOfRegion !== '' ?
+                                            <div>
+                                                <div className="quickTitle confirmedCo">Confirmed</div>
+
+                                                <div className="confirmedCo delta">
+                                                    {completeDetailsOfRegion.info3.deltaconfirmed !== '0' &&
+                                                        <><i className="material-icons  fontSize1 ">arrow_upward</i>
+                                                            {completeDetailsOfRegion.info3.deltaconfirmed}</>}
+                                                </div>
+
+                                                <div className="qvdc_nm">{completeDetailsOfRegion.info3.confirmed}</div>
+                                            </div> :
+                                            <LoaderComponent />}
+
+                                </div>
+                            </Waypoint>
+                            <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'act2', 'wayupanimation2')}>
+                                <div className="qdvc_ic" id="act2">
+
+                                    {
+                                        stateInfoLoader && completeDetailsOfRegion !== '' ?
+                                            <div>
+                                                <div className="quickTitle activeCo">Active</div>
+
+                                                <div className="activeCo delta">
+                                                    {/* {completeDetailsOfRegion.info3.deltaactive !== 0 &&<>
                                                 <i className="material-icons fontSize1">arrow_upward</i> {completeDetailsOfRegion.info3.deltaactive}</>} */}
-                                            </div>
+                                                </div>
 
-                                            <div className="qvdc_nm">{completeDetailsOfRegion.info3.active}</div>
-                                        </div> :
-                                        <LoaderComponent />}
-
-
-                            </div>
-                        </Waypoint>
-                        <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'rec2', 'wayupanimation3')}>
-                            <div className="qdvc_ic" id="rec2">
-                                {
-                                    stateInfoLoader && completeDetailsOfRegion !== '' ?
-                                        <div>
-                                            <div className="quickTitle recoveredCo">Recovered</div>
-
-                                            <div className="recoveredCo delta">
-                                                {completeDetailsOfRegion.info3.deltarecovered !== '0' &&
-                                                    <><i className="material-icons fontSize1">arrow_upward</i>
-                                                        {completeDetailsOfRegion.info3.deltarecovered}</>}
-                                            </div>
-
-                                            <div className="qvdc_nm">{completeDetailsOfRegion.info3.recovered}</div>
-                                        </div> :
-                                        <LoaderComponent />}
+                                                <div className="qvdc_nm">{completeDetailsOfRegion.info3.active}</div>
+                                            </div> :
+                                            <LoaderComponent />}
 
 
-                            </div>
-                        </Waypoint>
-                        <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'dea2', 'wayupanimation4')}>
-                            <div className="qdvc_ic" id="dea2">
-                                {
-                                    stateInfoLoader && completeDetailsOfRegion !== '' ?
-                                        <div>
-                                            <div className="quickTitle deceasedCo">Deceased</div>
-
-                                            <div className="deceasedCo delta">
-                                                {completeDetailsOfRegion.info3.deltadeaths !== '0' &&
-                                                    <><i className="material-icons fontSize1 ">arrow_upward</i>
-                                                        {completeDetailsOfRegion.info3.deltadeaths}</>}
-                                            </div>
-
-                                            <div className="qvdc_nm">{completeDetailsOfRegion.info3.deaths}</div>
-                                        </div> :
-                                        <LoaderComponent />}
-                            </div>
-                        </Waypoint>
-                    </div>
-
-                    {completeDetailsOfRegion.info2 !== '' && <div className="metaPop">
-                        <div className="qdvc_ic">
-                            <div>
-                                <div className="quickTitle subCo">Population</div>
-                                <div className="deceasedCo delta">
-                                    <i className="material-icons material-icons-outlined fontSize2 anyCo">groups</i>
                                 </div>
-                                <div className="qvdc_nm">{completeDetailsOfRegion.info2.meta.population}</div>
-                            </div>
+                            </Waypoint>
+                            <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'rec2', 'wayupanimation3')}>
+                                <div className="qdvc_ic" id="rec2">
+                                    {
+                                        stateInfoLoader && completeDetailsOfRegion !== '' ?
+                                            <div>
+                                                <div className="quickTitle recoveredCo">Recovered</div>
+
+                                                <div className="recoveredCo delta">
+                                                    {completeDetailsOfRegion.info3.deltarecovered !== '0' &&
+                                                        <><i className="material-icons fontSize1">arrow_upward</i>
+                                                            {completeDetailsOfRegion.info3.deltarecovered}</>}
+                                                </div>
+
+                                                <div className="qvdc_nm">{completeDetailsOfRegion.info3.recovered}</div>
+                                            </div> :
+                                            <LoaderComponent />}
+
+
+                                </div>
+                            </Waypoint>
+                            <Waypoint onEnter={this.addAnimationToWayUp.bind(this, 'dea2', 'wayupanimation4')}>
+                                <div className="qdvc_ic" id="dea2">
+                                    {
+                                        stateInfoLoader && completeDetailsOfRegion !== '' ?
+                                            <div>
+                                                <div className="quickTitle deceasedCo">Deceased</div>
+
+                                                <div className="deceasedCo delta">
+                                                    {completeDetailsOfRegion.info3.deltadeaths !== '0' &&
+                                                        <><i className="material-icons fontSize1 ">arrow_upward</i>
+                                                            {completeDetailsOfRegion.info3.deltadeaths}</>}
+                                                </div>
+
+                                                <div className="qvdc_nm">{completeDetailsOfRegion.info3.deaths}</div>
+                                            </div> :
+                                            <LoaderComponent />}
+                                </div>
+                            </Waypoint>
                         </div>
-                        <div className="qdvc_ic">
-                            <div>
-                                <div className="quickTitle subCo">Total Tests</div>
-                                <div className="deceasedCo delta">
-                                    {completeDetailsOfRegion.info3.deltadeaths !== '0' &&
-                                        <><i className="material-icons fontSize1 ">arrow_upward</i>
 
-
-                                            {completeDetailsOfRegion.info3.state === 'Total' ?                                             
-                                            completeDetailsOfRegion.info2.delta.tested.states.samples :
-                                                completeDetailsOfRegion.info2.delta.tested.samples}
-                                        </>
-                                    }
+                        {completeDetailsOfRegion.info2 !== '' && <div className="metaPop">
+                            <div className="qdvc_ic">
+                                <div>
+                                    <div className="quickTitle subCo">Population</div>
+                                    <div className="deceasedCo delta">
+                                        <i className="material-icons material-icons-outlined fontSize2 anyCo">groups</i>
+                                    </div>
+                                    <div className="qvdc_nm">{completeDetailsOfRegion.info2.meta.population}</div>
                                 </div>
-                                {typeof completeDetailsOfRegion.info2.total.tested !== undefined && <>{completeDetailsOfRegion.info3.state === 'Total' ? <div className="qvdc_nm">
-                                    {completeDetailsOfRegion.info2.total.tested.states.samples}</div> :
+                            </div>
+                            <div className="qdvc_ic">
+                                <div>
+                                    <div className="quickTitle subCo">Total Tests</div>
+                                    <div className="deceasedCo delta">
+                                        {completeDetailsOfRegion.info3.deltadeaths !== '0' &&
+                                            <><i className="material-icons fontSize1 ">arrow_upward</i>
 
-                                    <div className="qvdc_nm"> 
-                                    {completeDetailsOfRegion.info2.total.tested.samples}
-                                    </div>}
+
+                                                {completeDetailsOfRegion.info3.state === 'Total' ?
+                                                    completeDetailsOfRegion.info2.delta.tested.states.samples :
+                                                    completeDetailsOfRegion.info2.delta.tested.samples}
+                                            </>
+                                        }
+                                    </div>
+                                    {typeof completeDetailsOfRegion.info2.total.tested !== undefined && <>{completeDetailsOfRegion.info3.state === 'Total' ? <div className="qvdc_nm">
+                                        {completeDetailsOfRegion.info2.total.tested.states.samples}</div> :
+
+                                        <div className="qvdc_nm">
+                                            {completeDetailsOfRegion.info2.total.tested.samples}
+                                        </div>}
                                     </>}
+                                </div>
                             </div>
-                        </div>
-                    </div>}
+                        </div>}
 
 
 
-                </>
-                }
+                    </>
+                    }</>}
 
             </div>
         )
